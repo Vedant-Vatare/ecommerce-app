0c8c49feca9b38186acdfc2ba8e22e2dbbaf43d3
Vedant Vatare <vedantvatare73@gmail.com>
2025-11-08 15:05:10 +0530

    setup shared package in workspace

---
+++ packages/shared/index.js
@@ -0,0 +1,5 @@
+export * from './schemas/userSchema.js';
+export * from './schemas/productSchema.js';
+export * from './schemas/cartSchema.js';
+export * from './schemas/categorySchema.js';
+export * from './schemas/orderSchema.js';
